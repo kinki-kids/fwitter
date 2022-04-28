06c50f36da4c2d4eea5e85e942a2ce71edcaba10
takuma.matsuzaki <takuma.takuma0928@gmail.com>
2022-04-28 02:17:48 +0000

    post function+

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,8 @@
                 </div>
             </aside>
             <div class="col-sm-8">
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
                 {{-- 投稿一覧 --}}
                 @include('microposts.microposts')
             </div>
